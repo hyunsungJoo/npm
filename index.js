984abae298b84dfca0cbefbca55825f2f166f8c8
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-01-29 12:05:37 +0900

    refactoring

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(`
 
 
 
-
+/*
 class Random {
     constructor() {
         this.args = process.argv.slice(2);
@@ -37,7 +37,29 @@ class RandomPrint extends Random {
     }
 }
 
+
 const r = new RandomPrint();
 r.print();
+*/
 
+class Random {
+    constructor(...args) {
+        this.args = args;
+        this.length = args.length;
+    }
+}
 
+class RandomPrint extends Random {
+    constructor(a, ...args) {
+        super(...args);
+        const b = typeof a !== 'undefined' ? a : '발표자';
+	this.a = b;
+    }
+
+    print() {
+        console.log(this.a, this.args[Math.floor(Math.random() * this.length)]);
+    }
+}
+
+const r = new RandomPrint(...process.argv.slice(2));
+r.print();
